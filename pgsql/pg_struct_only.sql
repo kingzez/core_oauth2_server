a579eb66be2d78282fd6c6d494dfc3e46e62868a
wangzezhi <wangzezhi@xiaoyun.com>
2018-08-02 11:41:39 +0800

    Add db init script
    Test Plan: no
    
    Reviewers: yuyang
    
    Reviewed By: yuyang
    
    Maniphest Tasks: T23
    
    Differential Revision: http://review.xiaoyun.com/D47

---
+++ pgsql/pg_struct_only.sql
@@ -0,0 +1,104 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : PostgreSQL
+ Source Server Version : 100004
+ Source Host           : 127.0.0.1:5432
+ Source Catalog        : sso-server
+ Source Schema         : public
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 100004
+ File Encoding         : 65001
+
+ Date: 02/08/2018 00:43:33
+*/
+
+
+-- ----------------------------
+-- Table structure for AccessToken
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."AccessToken";
+CREATE TABLE "public"."AccessToken" (
+  "id" uuid NOT NULL,
+  "token" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "passportId" uuid NOT NULL,
+  "clientId" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "createdAt" int8,
+  "updatedAt" int8
+)
+;
+ALTER TABLE "public"."AccessToken" OWNER TO "w";
+
+-- ----------------------------
+-- Table structure for AuthorizationCode
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."AuthorizationCode";
+CREATE TABLE "public"."AuthorizationCode" (
+  "id" uuid NOT NULL,
+  "passportId" uuid NOT NULL,
+  "clientId" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "code" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "redirectUri" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "createdAt" int8,
+  "updatedAt" int8
+)
+;
+ALTER TABLE "public"."AuthorizationCode" OWNER TO "w";
+
+-- ----------------------------
+-- Table structure for Client
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."Client";
+CREATE TABLE "public"."Client" (
+  "id" uuid NOT NULL,
+  "name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "clientId" varchar COLLATE "pg_catalog"."default" NOT NULL,
+  "clientSecret" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "isTrusted" bool,
+  "createdAt" int8,
+  "updatedAt" int8
+)
+;
+ALTER TABLE "public"."Client" OWNER TO "w";
+
+-- ----------------------------
+-- Table structure for Passport
+-- ----------------------------
+DROP TABLE IF EXISTS "public"."Passport";
+CREATE TABLE "public"."Passport" (
+  "id" uuid NOT NULL,
+  "username" varchar(255) COLLATE "pg_catalog"."default",
+  "password" varchar(255) COLLATE "pg_catalog"."default",
+  "email" varchar(255) COLLATE "pg_catalog"."default",
+  "createdAt" int8,
+  "updatedAt" int8
+)
+;
+ALTER TABLE "public"."Passport" OWNER TO "w";
+
+-- ----------------------------
+-- Primary Key structure for table AccessToken
+-- ----------------------------
+ALTER TABLE "public"."AccessToken" ADD CONSTRAINT "AccessToken_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Uniques structure for table AuthorizationCode
+-- ----------------------------
+ALTER TABLE "public"."AuthorizationCode" ADD CONSTRAINT "AuthorizationCode_clientId_key" UNIQUE ("clientId");
+
+-- ----------------------------
+-- Primary Key structure for table AuthorizationCode
+-- ----------------------------
+ALTER TABLE "public"."AuthorizationCode" ADD CONSTRAINT "AuthorizationCode_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table Client
+-- ----------------------------
+ALTER TABLE "public"."Client" ADD CONSTRAINT "Client_pkey" PRIMARY KEY ("id");
+
+-- ----------------------------
+-- Primary Key structure for table Passport
+-- ----------------------------
+ALTER TABLE "public"."Passport" ADD CONSTRAINT "Passports_pkey" PRIMARY KEY ("id");
---
+++ src/util/init_db.ts
@@ -0,0 +1,50 @@
+import { v4 as uuidv4 } from 'uuid'
+
+import logger from './logger'
+import Passport from '../models/passport'
+import Client from '../models/client'
+
+const now:number = Date.now();
+
+function exit() {
+    setTimeout(() => {
+        return process.exit()
+    }, 2000);
+}
+
+// init Passport
+(async() => {
+    try {
+        let admin = await Passport.create({
+            id: uuidv4(),
+            username: 'admin',
+            password: 'admin',
+            email: 'admin@xiaoyun.com',
+            createdAt: now,
+            updatedAt: now,
+        })
+        logger.debug('created: ' + JSON.stringify(admin))
+    } catch (err) {
+        logger.debug(err)
+    }
+})();
+
+
+// init Client
+(async() => {
+    try {
+        let rmk = await Client.create({
+            id: uuidv4(),
+            name: 'rmk',
+            clientId: 'rmk',
+            clientSecret: 'rmk',
+            isTrusted: true,
+            createdAt: now,
+            updatedAt: now,
+        })
+        logger.debug('created: ' + JSON.stringify(rmk))
+        exit()
+    } catch (err) {
+        logger.debug(err)
+    }
+})();
